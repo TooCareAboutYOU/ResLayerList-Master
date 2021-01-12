e42dd69f1998c1169487164d99bf815d93b72548
zhangshuai <1175133706@qq.com>
2021-01-12 11:09:50 +0800

    Initial commit

---
+++ app/src/main/java/com/reslayer/listmain/MyApplication.java
@@ -0,0 +1,12 @@
+package com.reslayer.listmain;
+
+import android.app.Application;
+
+
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
